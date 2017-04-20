8f550bb54d99fc82dd8edf1ee1e40cac56103c65
Shadow <Shadow@DESKTOP-EUG89AS>
2017-04-20 16:03:30 +0200

    Removed unnecessary method call in app.js

--- werewolf/src/main/resources/static/js/app.js
+++ werewolf/src/main/resources/static/js/app.js
@@ -23,7 +23,6 @@ function connect() {
         stompClient.subscribe('/action/joinlobby/' + gamecode, function (messageOutput) {
             receiveMessage(JSON.parse(messageOutput.body));
         });
-        sendPrivateMessage({"action":"getallplayers"});
     });
 }
 
