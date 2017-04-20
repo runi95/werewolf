3816c1eb24274907ed059b0c9612c4419a6d1440
Shadow <Shadow@DESKTOP-EUG89AS>
2017-04-20 23:02:08 +0200

    Updated configurations to handle players leaving a lobby

---
+++ werewolf/src/main/java/com/werewolf/configurations/PresenceChannelInterceptor.java
@@ -0,0 +1,77 @@
+package com.werewolf.configurations;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptorAdapter;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+
+import com.werewolf.Messages.LobbyMessage;
+import com.werewolf.controllers.StompMessageController;
+import com.werewolf.entities.LobbyPlayer;
+import com.werewolf.entities.User;
+import com.werewolf.services.AccountService;
+import com.werewolf.services.LobbyPlayerService;
+
+public class PresenceChannelInterceptor extends ChannelInterceptorAdapter {
+
+	@Autowired
+    private SimpMessagingTemplate simpTemplate;
+	
+	@Autowired
+	private LobbyPlayerService lobbyPlayerService;
+	
+	@Autowired
+	private AccountService accountService;
+	
+    private final Log logger = LogFactory.getLog(PresenceChannelInterceptor.class);
+ 
+    @Override
+    public void postSend(Message<?> message, MessageChannel channel, boolean sent) {
+ 
+        StompHeaderAccessor sha = StompHeaderAccessor.wrap(message);
+ 
+        // ignore non-STOMP messages like heartbeat messages
+        if(sha.getCommand() == null) {
+            return;
+        }
+ 
+        String sessionId = sha.getSessionId();
+ 
+        switch(sha.getCommand()) {
+            case CONNECT:
+                logger.debug("STOMP Connect [sessionId: " + sessionId + "]");
+                break;
+            case CONNECTED:
+                logger.debug("STOMP Connected [sessionId: " + sessionId + "]");
+                break;
+            case DISCONNECT:
+            	disconnect(message);
+                logger.debug("STOMP Disconnect [sessionId: " + sessionId + "]");
+                break;
+            default:
+                break;
+ 
+        }
+    }
+    
+    private void disconnect(Message<?> message) {
+    	if(message != null && message.getHeaders().containsKey("simpUser")) {
+    		UsernamePasswordAuthenticationToken userToken = (UsernamePasswordAuthenticationToken) message.getHeaders().get("simpUser");
+    		User user = accountService.findByUsername(userToken.getName());
+    		LobbyPlayer lobbyPlayer = lobbyPlayerService.findByUser(user);
+    		
+    		List<LobbyMessage> lobbyMessages = new ArrayList<>();
+    		lobbyMessages.add(new LobbyMessage(Long.toString(lobbyPlayer.getUser().getId()), lobbyPlayer.getNickname(), "leave"));
+    		
+    		simpTemplate.convertAndSend("/action/lobbymessages/" + lobbyPlayer.getLobby().getGameId(), StompMessageController.convertArrayToJson(lobbyMessages));
+    	}
+    }
+}
--- werewolf/src/main/java/com/werewolf/configurations/WebSocketConfig.java
+++ werewolf/src/main/java/com/werewolf/configurations/WebSocketConfig.java
@@ -1,6 +1,8 @@
 package com.werewolf.configurations;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -10,6 +12,23 @@ import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 @EnableWebSocketMessageBroker
 public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
 
+    @Bean
+    public PresenceChannelInterceptor presenceChannelInterceptor() {
+        return new PresenceChannelInterceptor();
+    }
+ 
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.setInterceptors(presenceChannelInterceptor());
+    }
+ 
+    @Override
+    public void configureClientOutboundChannel(ChannelRegistration registration) {
+        registration.taskExecutor().corePoolSize(8);
+        registration.setInterceptors(presenceChannelInterceptor());
+    }
+ 
+	
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
         config.enableSimpleBroker("/action");
