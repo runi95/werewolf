fcdb0fa1a47a9bc6b2d8f9591e3d2e4a87745da6
Shadow <Shadow@DESKTOP-EUG89AS>
2017-05-23 12:50:57 +0200

    Added JoinLobbyMessage back

--- werewolf/src/main/java/com/werewolf/Messages/JoinLobbyMessage.java
+++ werewolf/src/main/java/com/werewolf/Messages/JoinLobbyMessage.java
@@ -1,22 +1,22 @@
 package com.werewolf.Messages;
 
 public class JoinLobbyMessage {
-    private String action;
-    private String playerid;
 
-    public String getAction() {
-        return action;
-    }
-    
-    public String getPlayerid() {
-    	return playerid;
-    }
-
-    public void setAction(String action) {
-        this.action = action;
-    }
-    
-    public void setPlayerid(String playerid) {
-    	this.playerid = playerid;
-    }
+	private String action, playerid;
+	
+	public void setPlayerid(String playerid) {
+		this.playerid = playerid;
+	}
+	
+	public String getPlayerid() {
+		return playerid;
+	}
+	
+	public void setAction(String action) {
+		this.action = action;
+	}
+	
+	public String getAction() {
+		return action;
+	}
 }
