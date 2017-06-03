86549592344c0ac50e295ea49b22f8f394744b98
runi95 <Runar.Serigstad@student.uib.no>
2017-06-03 02:59:42 +0200

    Removed unused imports

--- werewolf/src/main/java/com/werewolf/controllers/PreGameController.java
+++ werewolf/src/main/java/com/werewolf/controllers/PreGameController.java
@@ -1,19 +1,13 @@
 package com.werewolf.controllers;
 
 import com.werewolf.components.JoinLobbyFormValidator;
-import com.werewolf.data.JoinLobbyForm;
-import com.werewolf.entities.LobbyEntity;
-import com.werewolf.entities.User;
 import com.werewolf.services.AccountService;
 import com.werewolf.services.JoinLobbyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,8 +15,6 @@ import org.springframework.web.servlet.ModelAndView;
 
 import java.security.Principal;
 import java.util.Optional;
-
-import javax.validation.Valid;
 
 @PreAuthorize("isAuthenticated()")
 @Controller
