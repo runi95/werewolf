5f02a46ac5309f0e1a24ff9501424dcdf660edd1
runar <thegreatandpowerfulmetal@gmail.com>
2017-07-06 08:29:16 +0200

    Optimized imports

--- werewolf/src/main/java/com/werewolf/controllers/PreGameController.java
+++ werewolf/src/main/java/com/werewolf/controllers/PreGameController.java
@@ -6,18 +6,9 @@ import com.werewolf.services.JoinLobbyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mobile.device.Device;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.authentication.AnonymousAuthenticationToken;
-import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
-
-import java.security.Principal;
-import java.util.Optional;
 
 @PreAuthorize("isAuthenticated()")
 @Controller
