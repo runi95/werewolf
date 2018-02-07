9cd68706854c05e2bd2229bd4ae1fd06aafd06ae
runi95 <Runar.Serigstad@student.uib.no>
2018-02-07 16:13:06 +0100

    Implemented message sending

--- werewolf/src/main/java/com/werewolf/gameplay/rules/OneNight/VoteRuleEndsGame.java
+++ werewolf/src/main/java/com/werewolf/gameplay/rules/OneNight/VoteRuleEndsGame.java
@@ -1,17 +1,21 @@
 package com.werewolf.gameplay.rules.OneNight;
 
 import com.werewolf.Messages.PlayerMessage;
+import com.werewolf.Messages.PlayerMessageType;
 import com.werewolf.entities.LobbyEntity;
 import com.werewolf.entities.LobbyPlayer;
 import com.werewolf.gameplay.rules.VoteRule;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class VoteRuleEndsGame implements VoteRule {
 
     public List<PlayerMessage> vote(LobbyEntity lobbyEntity, LobbyPlayer voter, LobbyPlayer target, LobbyPlayer oldTarget, boolean flag) {
-        // First check that both the voter and target is alive and that the vote is valid.
 
+        // First check that both the voter and target is alive and that the vote is valid.
         if(lobbyEntity.getAlivePlayer(voter.getId()) == null)
             return null;
 
@@ -26,15 +30,43 @@ public class VoteRuleEndsGame implements VoteRule {
         oldTarget.setVotes(oldTarget.getVotes() - 1);
         target.setVotes(target.getVotes() + 1);
 
+        List<PlayerMessage> messages = new ArrayList<>();
+
+        Map<String, Object> messageMap = new HashMap<>();
+        Map<String, String> messageArgs = new HashMap<>();
+        messageArgs.put("playerid", voter.getId());
+        messageArgs.put("votedon", target.getId());
+        messageArgs.put("votes", Integer.toString(target.getVotes()));
+        messageMap.put("action", "vote");
+        messageMap.put("args", messageArgs);
+
+        PlayerMessage playerMessage = new PlayerMessage();
+        playerMessage.setPlayerMessageType(PlayerMessageType.BROADCAST);
+        playerMessage.setReceiverId(lobbyEntity.getGameId());
+        playerMessage.setMessageMap(messageMap);
+
         // Check if the target has enough votes to be lynched.
         if(target.getVotes() >= (lobbyEntity.getAliveCount() / 2) + 1) {
-            // Our target is definitely getting lynched.
+            // Our target is getting lynched.
+            lobbyEntity.addDeadPlayer(target);
 
-        } else {
+            Map<String, Object> lynchMap = new HashMap<>();
+            Map<String, String> lynchArgs = new HashMap<>();
+            lynchArgs.put("playerid", target.getId());
+            lynchArgs.put("playername", target.getNickname());
+            lynchArgs.put("playerrole", target.getRole().getName());
+            lynchArgs.put("playeralignment", target.getRole().getAlignment().getAlignmentName());
+            lynchMap.put("action", "killed");
+            lynchMap.put("args", lynchArgs);
 
+            PlayerMessage lynchMessage = new PlayerMessage();
+            lynchMessage.setPlayerMessageType(PlayerMessageType.BROADCAST);
+            lynchMessage.setReceiverId(lobbyEntity.getGameId());
+            lynchMessage.setMessageMap(lynchMap);
+            messages.add(lynchMessage);
         }
 
-        return null;
+        return messages;
     }
 
 }
