e9ee4ee3e17fb2cdedeb87acdc82c120a498c085
runi95 <Runar.Serigstad@student.uib.no>
2018-02-07 16:12:50 +0100

    Added name to OneNightUltimateWerewolfRuleSet

--- werewolf/src/main/java/com/werewolf/gameplay/rules/OneNight/OneNightUltimateWerewolfRuleSet.java
+++ werewolf/src/main/java/com/werewolf/gameplay/rules/OneNight/OneNightUltimateWerewolfRuleSet.java
@@ -5,7 +5,12 @@ import com.werewolf.gameplay.rules.RuleSet;
 
 public class OneNightUltimateWerewolfRuleSet implements RuleSet {
 
+    public static final String NAME = "One Night Ultimate Werewolf";
     Rule[] rules = new Rule[] { new VoteRuleEndsGame() };
+
+    public String getName() {
+        return NAME;
+    }
 
     public Rule[] getRules() {
         return rules;
